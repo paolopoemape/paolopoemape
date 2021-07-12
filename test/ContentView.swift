621438694bc463b92e43b69a4831f0cd62035c32
Paolo Poemape <paolopoemape@gmail.com>
2021-07-11 19:44:01 -0600

    first app commit

---
+++ test/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  test
+//
+//  Created by Paolo Poemape on 6/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Turtle Rock")
+            .font(.title)
+            .foregroundColor(.red)
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ContentView()
+        }
+    }
+}
---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Paolo Poemape on 6/11/21.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
